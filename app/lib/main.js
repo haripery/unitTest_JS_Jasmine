5887a24918892a20775b314fd43116054dcf5153
Hari <hariprasath.periyasamy@gmail.com>
2017-07-28 18:30:46 -0400

    file structure changed - not working

--- app/lib/main.js
+++ app/lib/main.js
@@ -6,6 +6,45 @@
 var sticyColor= "yellow";
 var notesNumber = null;
 
+var UiFramework = (function() {
+    function input(name, isAutoFocused) {
+      var element = document.createElement("input");
+      element.name = name;
+      element.autofocus = isAutoFocused;
+      return element;
+    }
+
+    function div(id, cssClass) {
+      var element = document.createElement("div");
+      element.id = id;
+      element.classList.add(cssClass);
+      return element;
+    }
+
+    function Button(options){
+    var button = $("<button>");
+    button.attr("id", options.id);
+    button.attr("type", "button");
+    button.attr("class", options.buttonClass);
+    button.text(options.label);
+    return button;
+};
+
+    return {
+      input: input,
+      div: div
+    }
+  })();
+
+var makeButton = function(options){
+    var button = $("<button>");
+    button.attr("id", options.id);
+    button.attr("type", "button");
+    button.attr("class", options.buttonClass);
+    button.text(options.label);
+    return button;
+};
+
 $(document).ready(function(){
     //Select Color
     $("button").on('click',function(event){
@@ -21,6 +60,14 @@ $(document).ready(function(){
     $("ul").on("click", "#deletesticky", function(event){
         deleteNotes(this);
     });
+    //
+    var button1= makeButton({id: "pink2", label: "+"});
+    var button2= makeButton({id: "orange2", label: "+"});
+    var button3= makeButton({id: "cyan2", label: "+"});
+
+    $('#colorButton').append(button1);
+    $('#colorButton').append(button2);
+    $('#colorButton').append(button3);
 });
 
 
---
+++ appTest/spec/testSpec.js
@@ -0,0 +1,41 @@
+console.log(' -----------------------------');
+console.log('| UNIT TEST FOR POST IT NOTES |');
+console.log(' -----------------------------');
+//Loading index.html components
+function loadHtml() {
+    $('.jasmine-fixture').load('../app/index.html #main-wrap', function (responseText, statusText, xhr) {
+        if (statusText === 'success') {
+            console.log(statusText);
+            $.get( "../app/lib/main.css", function( data ) {
+                $('head').append("<style>"+data+"</style>");//Loading CSS File and Run QUnit Test function
+            });
+        }
+        if (statusText === "error") {
+            console.log("An error occurred: " + xhr.status + " - " + xhr.statusText);
+        }
+    });
+}
+describe("Testing UI - POST IT NOTES", function() {
+    beforeAll(function() {
+        loadHtml();
+    });
+    it("Test case 1: Check Notes Empty", function() {
+        expect($('.post').length).toEqual(1);
+    });
+
+    it("Test case 2: check button function", function() {
+        console.log($('#sticky').attr('placeholder'));
+
+        expect(true).toEqual(true);
+    });
+
+    it("Test case 3:", function() {
+        console.log($('#sticky').attr('placeholder'));
+        expect(true).toEqual(true);
+    });
+
+    it("Test case 4:", function() {
+        console.log($('#sticky').attr('placeholder'));
+        expect(true).toEqual(true);
+    });
+});
